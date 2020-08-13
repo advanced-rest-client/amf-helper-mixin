a569c9a3c8c829714e553c85455b1f64bea19618
Carolina Wright <carolina.wright@mulesoft.com>
2020-08-13 15:13:21 -0300

    feat: Add new method to get lexical information

--- src/AmfHelperMixin.js
+++ src/AmfHelperMixin.js
@@ -1222,4 +1222,33 @@ export const AmfHelperMixin = (base) => class extends base {
       }
     });
   }
+
+  /**
+   * Get first appearance in lexical information
+   * @param {Object} value
+   * @return {String|undefined} order.
+   */
+  _getLexicalOrder(value) {
+    const sKey = this._getAmfKey(
+      this.ns.aml.vocabularies.docSourceMaps.sources
+    );
+    const sources = value[sKey];
+    if (!sources) {
+      return undefined;
+    }
+
+    const lKey = this._getAmfKey(
+      this.ns.aml.vocabularies.docSourceMaps.lexical
+    );
+    const lexical = sources[lKey];
+
+    if (!lexical || lexical.length === 0) {
+      return undefined;
+    }
+
+    const firstPosition = lexical[0];
+    const positionStart = firstPosition.indexOf('(') + 1;
+    const positionEnd = firstPosition.indexOf(',');
+    return firstPosition.substring(positionStart, positionEnd);
+  }
 };
