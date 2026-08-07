3a6497f37882006df905ad55fb891b18dd99481f
aloose_sfemu <aloose@salesforce.com>
2026-08-07 19:24:41 -0300

    @W-23597071 feat: add apiContract:parentTag term

--- src/Namespace.js
+++ src/Namespace.js
@@ -183,6 +183,7 @@ ns.aml.vocabularies.apiContract.allowReserved = `${contractKey}allowReserved`;
 ns.aml.vocabularies.apiContract.tag = `${contractKey}tag`;
 ns.aml.vocabularies.apiContract.tags = `${contractKey}tags`;
 ns.aml.vocabularies.apiContract.operationKind = `${contractKey}operationKind`;
+ns.aml.vocabularies.apiContract.parentTag = `${contractKey}parentTag`;
 ns.aml.vocabularies.shapes = {};
 ns.aml.vocabularies.shapes.key = `${ns.aml.vocabularies.key}shapes#`;
 const shapesKey = ns.aml.vocabularies.shapes.key;
